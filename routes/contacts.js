df0353386b6418f13723991663e478e72bb1906b
hmoisesgb <h.moises.gb@gmail.com>
2025-11-06 20:49:12 -0600

    added database connection

---
+++ routes/contacts.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const contactsController = require('../controllers/contacts');
+
+router.get('/', contactsController.getAll);
+router.get('/', contactsController.getSingle);
+
+module.exports= router;
